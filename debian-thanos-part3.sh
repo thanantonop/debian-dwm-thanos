52e3a26f151675f46c877d537124f08e4102984d
Thanos Antonopoulos <thanos@mountados.home>
2024-02-08 14:39:01 +0200

    debian-dwm-thanos

--- debian-thanos-part3.sh
+++ debian-thanos-part3.sh
@@ -6,6 +6,8 @@ cd ~/Pictures/backgrounds/nordic-backgrounds
 rm -rf .git
 cd ~
 cp kitty.conf ~/.config/kitty/
-cp picom.conf ~/.config/picom
+cp picom.conf ~/.config/picom.conf
 cp config.rasi ~/.config/rofi/
+mv debian-dwm-thanos ~/github
+cp ~/github/debian-dwm-thanos/.Xresources ~/
 cd ~
