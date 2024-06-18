58076e4038ec59efcf4c24c98ade9fe709f0577d
Thanos Antonopoulos <78595164+thanantonop@users.noreply.github.com>
2024-06-19 00:25:21 +0300

    debian-dwm-thanos
    debian-dwm-thanos

--- debian-thanos-part2.sh
+++ debian-thanos-part2.sh
@@ -6,9 +6,6 @@ cd ~/github
 git clone https://github.com/thanantonop/dwm-thanos.git
 git clone https://github.com/thanantonop/st-thanos.git
 git clone https://github.com/thanantonop/dmenu-thanos.git
-#git clone https://github.com/thanantonop/surf-thanos.git
-#git clone https://github.com/thanantonop/tabbed-thanos.git
-#git clone https://github.com/neovim/neovim.git
 cd dwm-thanos
 sudo make clean install
 cd ..
@@ -18,25 +15,13 @@ cd ..
 cd dmenu-thanos
 sudo make clean install
 cd ..
-#cd surf-thanos
-#sudo make clean install
-#cd ..
-#cd tabbed-thanos
-#sudo make clean install
-#cd ..
-#mv neovim neovim_stable
 cd ~
 sudo cp dwm.desktop /usr/share/xsessions/
 sudo ufw enable
 sudo apt install python3 python3-pip lsd dunst xbacklight stterm mpv cmus audacious geany meld libreoffice libreoffice-gtk3 abiword emacs gimp stellarium transmission-gtk evince keepassxc inkscape darktable krita filezilla qimgv blueman lxappearance flatpak thunar thunar-volman thunar-archive-plugin dmenu acpi numlockx playerctl scrot sysstat xbindkeys feh galculator file-roller gvfs jq x11-utils tumbler vlc ttf-mscorefonts-installer chromium cherrytree xournal tree redshift synaptic l3afpad conky-all cups gvfs-backends lm-sensors fonts-powerline fonts-font-awesome fonts-ubuntu fonts-ubuntu-console fonts-liberation fonts-liberation2 fonts-terminus -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 cd ~
-#cd ~/github/neovim_stable
-#git checkout stable
-#sudo apt-get install ninja-build gettext cmake unzip curl -y
-#make CMAKE_BUILD_TYPE=RelWithDebInfo
-#sudo make install
-#cd ~
+sudo apt-get install ninja-build gettext cmake unzip curl -y
 echo "options snd_hda_intel power_save=0" | sudo tee -a /etc/modprobe.d/audio_disable_powersave.conf
 cd ~
 mkdir Downloads
@@ -45,15 +30,6 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.z
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
 unzip FiraCode.zip -d ~/.fonts
 unzip Meslo.zip -d ~/.fonts
-cd ~
-#git clone https://github.com/ChrisTitusTech/neovim.git
-#cd ~/neovim/
-#./setup.sh
-#cd ~
-#sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-#echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-#sudo apt update -y
-#sudo apt install brave-browser -y
 cd ~
 sudo ln -s /usr/local/bin/st /usr/bin/gnome-terminal
 cd ~
