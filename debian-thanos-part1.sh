f191abbf170922e6f5a1c85763941051ba0670b1
Thanos Antonopoulos <78595164+thanantonop@users.noreply.github.com>
2024-11-26 16:33:22 +0200

    debian-dwm-thanos
    debian-dwm-thanos

--- debian-thanos-part1.sh
+++ debian-thanos-part1.sh
@@ -1,5 +1,6 @@
 sudo apt update -y
 sudo apt install packaging-dev build-essential automake autoconf git vim ranger htop nmap neofetch sudo efibootmgr -y
+sudo apt install lightdm x11-xserver-utils pulseaudio xorg xcb libx11-xcb-dev libxcb-res0-dev libimlib2-dev arandr network-manager alsa-utils dmenu zoxide trash-cli ufw libglib2.0-dev libgtk-3-dev libgcr-3-dev libwebkit2gtk-4.0-dev firefox-esr -y
 cd ~
 git clone https://github.com/ChrisTitusTech/debian-titus
 cd debian-titus
