c9cd6dde64f6df3a811cd147de669507a9e16354
Thanos Antonopoulos <thanos@tinos.home>
2024-12-03 23:37:29 +0200

    debian-dwm-thanos

--- debian-xfce-only.sh
+++ debian-xfce-only.sh
@@ -18,18 +18,21 @@ sudo apt purge dwm stterm dmenu rofi arandr dunst i3lock arc-theme papirus-icon-
 # Install required packages on the XFCE
 sudo apt install packaging-dev build-essential automake autoconf git vim ranger htop nmap neofetch sudo efibootmgr -y
 sudo apt install firmware-iwlwifi -y
-sudo apt install lightdm x11-xserver-utils pulseaudio xorg xcb libx11-xcb-dev libxcb-res0-dev libimlib2-dev network-manager alsa-utils zoxide trash-cli ufw libglib2.0-dev libgtk-3-dev libgcr-3-dev libwebkit2gtk-4.0-dev firefox-esr -y
+sudo apt install lightdm x11-xserver-utils xorg xcb libx11-xcb-dev libxcb-res0-dev libimlib2-dev network-manager alsa-utils zoxide trash-cli ufw libglib2.0-dev libgtk-3-dev libgcr-3-dev libwebkit2gtk-4.0-dev firefox-esr -y
 sudo apt install python3 python3-pip lsd bat neovim bpytop mpv cmus audacious geany meld libreoffice libreoffice-gtk3 claws-mail abiword gnumeric emacs gimp stellarium transmission-gtk evince keepassxc inkscape darktable krita filezilla qimgv blueman flatpak thunar thunar-volman thunar-archive-plugin acpi numlockx playerctl scrot sysstat feh galculator file-roller gvfs jq x11-utils tumbler vlc ttf-mscorefonts-installer chromium cherrytree xournal tree redshift synaptic conky-all cups gvfs-backends lm-sensors alacritty kitty fish mugshot fontforge fonts-crosextra-carlito fonts-crosextra-caladea fonts-powerline fonts-font-awesome fonts-ubuntu fonts-ubuntu-console fonts-liberation fonts-liberation2 fonts-terminus -y
-sudo apt install multitail tree zoxide trash-cli fzf bash-completion -y
+sudo apt install multitail tree zoxide trash-cli fzf bash-completion ffmpeg default-jdk git wget nano vim htop locate p7zip p7zip-full unzip dos2unix net-tools ethtool bind9-utils curl wget wireshark -y
 sudo apt install network-manager-gnome lightdm-gtk-greeter-settings celluloid strawberry timeshift gdebi gparted gufw thunderbird handbrake audacity hexchat -y
 sudo apt install qemu-system libvirt-daemon-system virt-manager -y
 sudo apt install task-desktop task-xfce-desktop -y
 sudo apt install xfce4 xfce4-goodies catfish -y
+sudo apt install libspa-0.2-bluetooth wireplumber pipewire-media-session- -y
+sudo apt install pipewire-pulse pipewire-jack pipewire-alsa -y
 sudo apt autoremove -y
 sudo apt autoclean -y 
 sudo apt autopurge -y
 sudo unlink /usr/bin/gnome-terminal
 sudo rm -rf /usr/share/xsessions/dwm.desktop 
+systemctl --user --now enable wireplumber.service 
 
 # Remove / Install configuration files
 rm -rf ~/.config/rofi
